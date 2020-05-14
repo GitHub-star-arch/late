ae94b96658b87ad1fa58bae8f3d9febaa2f4d645
GitHub-star-arch <nithinraggov@gmail.com>
2020-05-14 09:28:30 -0400

    late

--- Bird.js
+++ Bird.js
@@ -2,12 +2,21 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.pimage = loadImage("sprites/smoke.png");
+    this.arr1=[]
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-
     super.display();
+    if(this.body.speed>10 || this.body.position.x>200){
+      var position = [this.body.position.x,this.body.position.y]
+    this.arr1.push(position);
+    }
+    for(var i=0;i<this.arr1.length;i++){
+      image(this.pimage,this.arr1[i][0],this.arr1[i][1])
+    }
+    console.log(this.body.speed);
   }
 }
